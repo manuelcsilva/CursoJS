df72fc2a93da71d4c60c927f6517df0991f9d62e
Manuel Silva <manuelcmsilva@hotmail.com>
2024-07-21 18:02:49 +0100

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,13 +10,40 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        img.setAttribute('id', 'foto') 
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto_bebe_m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto_jovem_m.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto_adulto_m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto_idoso_m.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto_bebe_f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto_jovem_f.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto_adulto_f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto_idoso_f.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
